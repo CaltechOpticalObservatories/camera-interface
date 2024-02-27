b6ea06da9e6acb6563ffdd0d1efcc946269f27e4
Don Neill <neilljd@gmail.com>
2024-02-27 11:16:13 -0800

    Adjust wait time in hwait_for_readout in archon.cpp

--- camerad/archon.cpp
+++ camerad/archon.cpp
@@ -5207,7 +5207,7 @@ namespace Archon {
         message << "waiting for new frame: current frame=" << this->lastframe << " current buffer=" << this->frame.index+1;
         logwrite(function, message.str());
 
-        usleep( 500 );  // tune for size of window
+        usleep( 520 );  // tune for size of window
 
         this->frame.index += 1;
 
