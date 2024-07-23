64313cde7bfa65a56a1488755f6e3510db76ba08
Michael Langmayr <michael.langmayr@gmail.com>
2024-07-23 16:37:46 -0700

    add CI workflow

---
+++ .github/install-deps.sh
@@ -0,0 +1,6 @@
+sudo apt-get update &&
+sudo apt-get install libccfits-dev &&
+sudo apt-get install libcfitsio-dev &&
+sudo apt-get install libcurl4-openssl-dev &&
+sudo apt-get install libxerces-c-dev &&
+sudo apt-get install libopencv-dev
