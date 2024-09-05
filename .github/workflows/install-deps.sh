171146e0b6893c291177118a88a7c22b6faf3c47
Mike Langmayr <michael.langmayr@gmail.com>
2024-09-05 15:12:50 -0700

    prototyping in progress

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -2,4 +2,5 @@ sudo apt-get update &&
 sudo apt-get install libccfits-dev &&
 sudo apt-get install libcfitsio-dev &&
 sudo apt-get install libcurl4-openssl-dev &&
-sudo apt-get install libgtest-dev
+sudo apt-get install libgtest-dev &&
+sudo apt-get install libzmqpp-dev
