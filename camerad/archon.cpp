873fc9a7b349a3de5c3d3ca8d807b094d841428c
Mike Langmayr <michael.langmayr@gmail.com>
2024-09-11 12:45:14 -0700

    prototyping in progress

--- camerad/archon.cpp
+++ camerad/archon.cpp
@@ -2826,7 +2826,7 @@ namespace Archon {
           int bytes_ready = 236;
 
           // Read header
-          retval = this->archon.Read(buffer, 36);
+          retval = this->archon.Read(buffer, 20);
 
           // if (strncmp(header, "<QF", 3) == 0) {
 
