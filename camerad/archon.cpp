55f2e52a8d6a0dec8a677c400a5820374c0c27fd
Mike Langmayr <michael.langmayr@gmail.com>
2024-09-18 16:18:07 -0700

    prototyping in progress

--- camerad/archon.cpp
+++ camerad/archon.cpp
@@ -2834,7 +2834,7 @@ namespace Archon {
             // memcpy(ptr_image, buffer + 36, 200);
             // ptr_image += retval;
 
-            totalbytesread = retval - header_size;
+            totalbytesread = retval;
             // logwrite( function, "copied " + std::to_string(totalbytesread) + " to image pointer");
             // }
 
