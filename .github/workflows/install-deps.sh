44b5fb20f1a24183f91150357657d2ff67542ffb
Mike Langmayr <michael.langmayr@gmail.com>
2024-10-16 08:57:08 -0700

    gh workflow: add cppzmq to install dependencies

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -3,4 +3,4 @@ sudo apt-get install libccfits-dev &&
 sudo apt-get install libcfitsio-dev &&
 sudo apt-get install libcurl4-openssl-dev &&
 sudo apt-get install libgtest-dev &&
-sudo apt-get install libzmq3-dev cppzmq
+sudo apt-get install libzmq3-dev cppzmq-dev
