ae44711b7e4a4b5b72edd76ab2f665ad7d616170
COO Software <coo_github@logs.astro.caltech.edu>
2025-05-13 15:32:12 -0700

    log in separate thread

--- utils/logentry.cpp
+++ utils/logentry.cpp
@@ -17,6 +17,31 @@ unsigned int nextday = 86410; /// number of seconds until a new day
 bool to_stderr = true; /// write to stderr by default
 std::string tmzone_log; /// optional time zone for logging
 
+std::queue<std::string> log_queue;
+std::condition_variable log_cv;
+std::atomic<bool> logger_running{true};
+std::thread logger_thread;
+
+
+void logger_worker() {
+    while (logger_running || !log_queue.empty()) {
+        std::unique_lock<std::mutex> lock(loglock);
+        log_cv.wait(lock, [] { return !log_queue.empty() || !logger_running; });
+
+        while (!log_queue.empty()) {
+            std::string msg = std::move(log_queue.front());
+            log_queue.pop();
+            lock.unlock();
+
+            if (filestream.is_open()) filestream << msg;
+            if (to_stderr) std::cerr << msg;
+
+            lock.lock();
+        }
+        if (filestream.is_open()) filestream.flush();
+    }
+}
+
 
 /***** init_log ***************************************************************/
 /**
@@ -38,87 +63,47 @@ long init_log(std::string name, std::string logpath, std::string logstderr, std:
     int year, mon, mday, hour, min, sec, usec;
     long error = 0;
 
-    to_stderr = (logstderr == "false" ? false : true); // should logwrite also print to stderr?
-
+    to_stderr = (logstderr != "false");
     tmzone_log = logtmzone;
 
     if ((error = get_time(tmzone_log, year, mon, mday, hour, min, sec, usec))) return error;
 
-    // assemble log file name from the passed-in arguments and current date
-    //
     filename << logpath << "/" << name << "_" << std::setfill('0')
-            << std::setw(4) << year
-            << std::setw(2) << mon
-            << std::setw(2) << mday << ".log";
+             << std::setw(4) << year << std::setw(2) << mon << std::setw(2) << mday << ".log";
 
-    // number of seconds until a new day
-    //
-    nextday = (unsigned int) (86410 - hour * 3600 - min * 60 - sec);
+    nextday = static_cast<unsigned int>(86410 - hour * 3600 - min * 60 - sec);
 
-    // open the log file stream for append
-    //
     try {
         filestream.open(filename.str(), std::ios_base::app);
-    } catch (const std::filesystem::filesystem_error &e) {
-        message.str("");
-        message << "ERROR " << filename.str() << ": " << e.what() << ": " << e.code().value();
-        logwrite(function, message.str());
-        return 1;
-    }
-    catch (...) {
-        message.str("");
-        message << "ERROR: opening log file " << filename.str() << ": " << std::strerror(errno);
-        logwrite(function, message.str());
+    } catch (...) {
+        std::cerr << "ERROR: opening log file failed\n";
         return 1;
     }
 
-    // If I am the owner then make sure the permissions are set correctly.
-    // Only the owner can change permissions. Even if I have write access,
-    // if I'm not the owner then I can't change the permissions.
-    //
     if (is_owner(filename.str())) {
         try {
-            // remove all permissions
-            //
-            std::filesystem::permissions(filename.str(),
-                                         std::filesystem::perms::all,
-                                         std::filesystem::perm_options::remove
-            );
-
-            // add back permissions rw-rw-r-- (664)
-            //
+            std::filesystem::permissions(filename.str(), std::filesystem::perms::all,
+                                         std::filesystem::perm_options::remove);
             std::filesystem::permissions(filename.str(),
                                          std::filesystem::perms::owner_read |
                                          std::filesystem::perms::owner_write |
                                          std::filesystem::perms::group_read |
                                          std::filesystem::perms::group_write |
                                          std::filesystem::perms::others_read,
-                                         std::filesystem::perm_options::add
-            );
-        } catch (const std::filesystem::filesystem_error &e) {
-            message.str("");
-            message << "ERROR " << filename.str() << ": " << e.what() << ": " << e.code().value();
-            logwrite(function, message.str());
+                                         std::filesystem::perm_options::add);
+        } catch (...) {
+            std::cerr << "ERROR: setting permissions failed\n";
             return 1;
         }
     }
 
-    // If I do not have write permission then that is a fatal error
-    //
-    if (!has_write_permission(filename.str())) {
-        message.str("");
-        message << "ERROR: no write permission for log file " << filename.str();
-        logwrite(function, message.str());
+    if (!has_write_permission(filename.str()) || !filestream.is_open()) {
+        std::cerr << "ERROR: no write permission or file not open\n";
         return 1;
     }
 
-    if (!filestream.is_open()) {
-        message.str("");
-        message << "ERROR: log file " << filename.str() << " not open";
-        logwrite(function, message.str());
-        return 1;
-    }
-
+    logger_running = true;
+    logger_thread = std::thread(logger_worker);
     return 0;
 }
 
@@ -132,10 +117,10 @@ long init_log(std::string name, std::string logpath, std::string logstderr, std:
  *
  */
 void close_log() {
-    if (filestream.is_open()) {
-        filestream.flush();
-        filestream.close();
-    }
+    logger_running = false;
+    log_cv.notify_one();
+    if (logger_thread.joinable()) logger_thread.join();
+    if (filestream.is_open()) filestream.close();
 }
 
 /***** close_log **************************************************************/
@@ -156,18 +141,14 @@ void close_log() {
  */
 void logwrite(const std::string &function, const std::string &message) {
     std::stringstream logmsg;
-
-    std::lock_guard<std::mutex> lock(loglock); // lock mutex to protect from multiple access
-
-    std::string timestamp = get_timestamp(tmzone_log); // get the current time (defined in utilities.h)
-
+    std::string timestamp = get_timestamp(tmzone_log);
     logmsg << timestamp << "  (" << function << ") " << message << "\n";
 
-    if (filestream.is_open()) {
-        filestream << logmsg.str(); // send to the file stream (if open)
-        filestream.flush();
+    {
+        std::lock_guard<std::mutex> lock(loglock);
+        log_queue.push(logmsg.str());
     }
-    if (to_stderr) std::cerr << logmsg.str(); // send to standard error if requested
+    log_cv.notify_one();
 }
 
 /***** logwrite ***************************************************************/
