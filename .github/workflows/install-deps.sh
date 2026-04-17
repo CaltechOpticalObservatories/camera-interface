a2a89573144999e92ef2e7cd2dd043567befd808
Prakriti Gupta <pgupta@caltech.edu>
2026-04-17 10:34:24 -0700

    Update CMake workflow for instruments

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -1,5 +1,12 @@
+set -euo pipefail
+
 sudo apt-get update &&
 sudo apt-get install libccfits-dev &&
 sudo apt-get install libcfitsio-dev &&
 sudo apt-get install libcurl4-openssl-dev &&
-sudo apt-get install libgtest-dev
+sudo apt-get install libgtest-dev &&
+sudo build-essential &&
+sudo cmake &&
+sudo ninja-build &&
+sudo nlohmann-json3-dev &&
+sudo libzmq3-dev
