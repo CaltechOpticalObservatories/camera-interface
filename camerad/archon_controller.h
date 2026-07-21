902b8d564180206dfc06329339ab147e03e32834
Elijah AB <elijahab@caltech.edu>
2026-07-21 16:11:15 -0700

    fix msgref init issue. Mike L

--- camerad/archon_controller.h
+++ camerad/archon_controller.h
@@ -294,7 +294,7 @@ namespace Camera {
       std::atomic_flag archon_busy = ATOMIC_FLAG_INIT;  //!< indicates a thread is accessing Archon
       bool is_firmwareloaded;
       std::string firmware;
-      int msgref;
+      int msgref{0};       //!< message reference id, incremented per command; MUST start initialized
       std::string backplaneversion;
       std::vector<int> modtype;             //!< type of each module from SYSTEM command
       std::vector<std::string> modversion;  //!< version of each module from SYSTEM command
